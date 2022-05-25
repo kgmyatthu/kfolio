0809f8d38c1ae5b6d5ac05d48697be985963fc31
kgmyatthu <2962000kaungmyatthu@gmail.com>
2022-05-25 23:12:02 +0630

    fix: broken build

--- component/intro/intro.component.tsx
+++ component/intro/intro.component.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import styles from './intro.module.css';
 import Image from "next/image";
-import profileImg from '../../assets/img/profile.jpeg';
+import profileImg from '../../assets/img/profile.png';
 import { classNameHelper as css } from "../../utils/utils";
 import ShadowButton from "../buttons/shadowButton.component";
 import { useRef } from "react";
