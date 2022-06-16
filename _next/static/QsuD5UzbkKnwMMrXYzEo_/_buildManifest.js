e7d1491996953cc9494b7bc4a798ca0ae709f899
kgmyatthu <kgmyatthu@users.noreply.github.com>
2022-06-16 20:26:07 +0000

    deploy: 531a7f92cd7ee2f0dd084a1e40f5f63d7060cafa

--- _next/static/QsuD5UzbkKnwMMrXYzEo_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/06d5fa16156fb1c7.css","static/chunks/pages/index-b916693cd0e6385a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
