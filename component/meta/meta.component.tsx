80502b99353487a2784b19a77e787a63a0abb1b1
kgmyatthu <2962000kaungmyatthu@gmail.com>
2022-09-14 02:33:17 +0630

    fix: meta tag

--- component/meta/meta.component.tsx
+++ component/meta/meta.component.tsx
@@ -19,11 +19,12 @@ const MetaConfig: NextPage<MetaProps> = ({title, description, imagePath, url}) =
       <meta property="og:description" content={description}/>
       <meta property="og:image" content={imagePath}/>
 
-      <meta property="twitter:card" content={title}/>
+      <meta property="twitter:card" content="summary_large_image"/>
       <meta property="twitter:url" content={url}/>
       <meta property="twitter:title" content={title}/>
       <meta property="twitter:description" content={description}/>
       <meta property="twitter:image" content={imagePath}/>
+
    </Head>
   )
 }
